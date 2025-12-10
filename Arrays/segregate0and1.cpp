84b7a64a0d542c7d167c6741b04a6104b2fa85f9
Om Jaiswal <omjaiswalllll08@gmail.com>
2025-12-10 22:18:47 +0530

    segregate 0 and 1 qn for two pointer approach where i sets place for 0

---
+++ Arrays/segregate0and1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of vector here : ";
+    cin>>n;
+    vector <int> v(n);
+    cout<<"Enter only 0 and 1 as elements in the vector : ";
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>>v[i];
+    }
+    // TIME COMPLEXITY OF O(2n) : 
+    // int c1=0,c2=0;
+    // for(int i =0 ;i< n;i++)
+    // {
+    //     if(v[i]==0) c1++;
+    //     else c2++;
+    // }
+    // for(int i = 0 ; i <c1; i++)
+    // {
+    //     v[i]=0;
+    // }
+    // for(int i = c2 ; i < n; i++)
+    // {
+    //     v[i]=1;
+    // }
+    // cout<<"Printing segregated vector now : ";
+    // for(int e : v)
+    // {
+    //     cout<<e<<" ";
+    // }
+    //TIME COMPLEXITY OF O(n) : 
+    int i  = 0;
+    int j = 0;
+    while(j<n)
+    {
+        if(v[j] == 0) 
+        {
+            swap (v[i],v[j]);
+            i++;
+        }
+        j++;
+    }
+    for(int e : v)
+    {
+        cout<<e<<" ";
+    }
+}
