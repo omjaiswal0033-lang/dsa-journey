4bc448e93b398f057c8d9b9cf0425a3443533e1c
Om Jaiswal <omjaiswal0033@gmail.com>
2026-01-01 18:29:12 +0530

    sets and unordered set

---
+++ C++ STL/sets.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //its map without values from key values pair.   
+    //set <string> s; // here all the elements are stored in ascending sorted order.
+    //s.insert("4"); // insertion takes log(n)
+    //s.find("4") // finding takes log(n) , it returns a iterator to 4.
+    //s.erase("4"); // takes log(n)
+    // ordered set is also implemented via red black trees.
+    //UNORDERED SET
+    // its implemented thru hash.
+    //now insertion finding erasing takes O(1).
+}
