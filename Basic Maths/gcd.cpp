79532a38592d2452330e9e57cb630af5f8e6a1b0
Om Jaiswal <omjaiswal0033@gmail.com>
2026-01-01 16:27:06 +0530

    gcd shortcut

--- Basic Maths/gcd.cpp
+++ Basic Maths/gcd.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 int main()
 {
-    int x=81,y=17;
-    while(y!= 0)
-    {
-        int c = x%y;
-        x = y;
-        y = c;
-    }
-    cout<<"Gcd is : "<<x;
+    int x=81,y=9;
+    // while(y!= 0)
+    // {
+    //     int c = x%y;
+    //     x = y;
+    //     y = c;
+    // }
+    // cout<<"Gcd is : "<<x;
+
+
+    // optimal approach : 
+    cout<<__gcd(x,y);
 }
